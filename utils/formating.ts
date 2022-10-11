ea3a9272b1c9c10dadfa6f3fa6d762ceca20dc7b
David <datogio@gmail.com>
2022-10-11 15:49:13 +0400

    fix: first letter capitalizer

--- utils/formating.ts
+++ utils/formating.ts
@@ -1,7 +1,9 @@
 export const returnCapitalizedFirstLetter = (word: string): string => {
   const contains = /[^\u0000-\u00ff]/.test(word);
 
-  if (!contains) return word[0];
+  if (!word) return '';
 
-  return word[0].toLocaleLowerCase();
+  if (contains) return word[0];
+
+  return word[0].toUpperCase();
 };
