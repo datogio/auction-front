6698ebb05875f374af41f0a7473d68a83865b18c
David <datogio@gmail.com>
2022-10-11 23:25:57 +0400

    feat: introduce <Container /> layout

---
+++ stories/layouts/Container.stories.tsx
@@ -0,0 +1,8 @@
+import { Container as ContainerLayout } from '../../components/layouts';
+
+export default {
+  title: 'Layouts/Container',
+  component: ContainerLayout,
+};
+
+export const Container = () => <ContainerLayout />;
