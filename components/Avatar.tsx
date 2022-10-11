425c248ba3cdac541a890782d462b378215fca58
David <datogio@gmail.com>
2022-10-11 14:15:02 +0400

    feat: implement <Avatar /> random colors

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -7,6 +7,21 @@ export interface IAvatar {
   image?: image.Response;
 }
 
+const COLORS = [
+  'bg-green-500',
+  'bg-blue-500',
+  'bg-red-500',
+  'bg-cyan-500',
+  'bg-purple-500',
+];
+
+const COLOR_MAPS: Record<number, string> = COLORS.reduce(
+  (acc, color, index) => {
+    return (acc = { ...acc, [index]: color });
+  },
+  {}
+);
+
 const Avatar = ({ firstName, lastName, image }: IAvatar) => {
   return (
     <div className="relative w-20 h-20">
@@ -19,7 +34,11 @@ const Avatar = ({ firstName, lastName, image }: IAvatar) => {
           className="rounded-full"
         />
       ) : (
-        <div className="w-[100%] h-[100%] bg-green-500 rounded-full flex items-center justify-center text-white text-2xl">
+        <div
+          className={`${
+            COLOR_MAPS[Math.floor(Math.random() * COLORS.length)]
+          } w-[100%] h-[100%] rounded-full flex items-center justify-center text-white text-2xl`}
+        >
           {firstName[0].toUpperCase()}
           {lastName[0].toUpperCase()}
         </div>
