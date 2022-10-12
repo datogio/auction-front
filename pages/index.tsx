c8ae395c170c4398f660ff1ae081f67bed493216
David <datogio@gmail.com>
2022-10-12 15:32:42 +0400

    style: implement home page stories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-import { PageLayout } from '../components/layouts';
+import { PageLayout, Container } from '../components/layouts';
+import { AvatarWithName } from '../components';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <PageLayout title="Components" description="Home page description">
-      <main>Page Main</main>
+      <main>
+        <Container color="BLACK" padding="SOLID">
+          <Container color="GRAY" padding="MEDIUM">
+            <AvatarWithName firstName="David" lastName="Giorgadze" />
+          </Container>
+        </Container>
+      </main>
     </PageLayout>
   );
 };
