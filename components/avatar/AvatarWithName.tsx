2405084f9835f7e6b71319e6dfc3fc2c0cb44934
David <datogio@gmail.com>
2022-10-12 15:25:50 +0400

    refactor: pass firstName, lastName and image props

--- components/avatar/AvatarWithName.tsx
+++ components/avatar/AvatarWithName.tsx
@@ -1,11 +1,18 @@
-import type { ReactNode } from 'react';
+import { Avatar, Name } from '../../components';
 
-interface IAvatarWithName {
-  children: ReactNode;
+export interface IAvatarWithName {
+  firstName: string;
+  lastName: string;
+  image?: image.Response;
 }
 
-const AvatarWithName = ({ children }: IAvatarWithName) => {
-  return <div className="flex flex-col items-center space-y-2">{children}</div>;
+const AvatarWithName = ({ firstName, lastName, image }: IAvatarWithName) => {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <Avatar firstName={firstName} lastName={lastName} image={image} />
+      <Name name={firstName} />
+    </div>
+  );
 };
 
 export default AvatarWithName;
--- stories/components/avatar/AvatarWithName.stories.tsx
+++ stories/components/avatar/AvatarWithName.stories.tsx
@@ -1,29 +1,25 @@
-import {
-  AvatarWithName as AvatarWithNameComponent,
-  Avatar,
-  FirstAndLastName,
-  Name,
-} from '../../../components';
+import { AvatarWithName as AvatarWithNameComponent } from '../../../components';
 import { Container } from '../../../components/layouts';
-import type { IAvatar } from '../../../components/avatar/Avatar';
+import type { IAvatarWithName } from '../../../components/avatar/AvatarWithName';
 
 export default {
   title: 'Components/Avatar',
   moponent: AvatarWithNameComponent,
 };
 
-const avatarArgs: IAvatar = {
+const avatarArgs: IAvatarWithName = {
   firstName: 'David',
   lastName: 'Giorgadze',
 };
 
-export const AvatarWithName = () => (
+export const AvatarWithName = (args: IAvatarWithName) => (
   <Container padding="SOLID" color="BLACK">
-    <AvatarWithNameComponent>
-      <Avatar firstName={avatarArgs.firstName} lastName={avatarArgs.lastName} />
-      <FirstAndLastName>
-        <Name name={avatarArgs.firstName} />
-      </FirstAndLastName>
-    </AvatarWithNameComponent>
+    <AvatarWithNameComponent
+      firstName={args.firstName}
+      lastName={args.lastName}
+      image={args.image}
+    />
   </Container>
 );
+
+AvatarWithName.args = { firstName: 'David', lastName: 'Giorgadze' };
