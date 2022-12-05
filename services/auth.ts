a684f21adfd2ed992612801baa761f9e322d20ad
David <datogio@gmail.com>
2022-12-05 16:16:12 +0400

    feat: implement sign in service

---
+++ services/auth.ts
@@ -0,0 +1,24 @@
+const headers = new Headers();
+headers.set('Accept', 'application/json');
+headers.set('Content-Type', 'application/json');
+
+export const signIn = (identifier: string, password: string) => {
+  return new Promise(
+    (
+      resolve: (message: 'success') => void,
+      reject: (message: 'Ivalid email or password') => void
+    ) => {
+      fetch(`http://localhost:3000/api/auth`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ identifier, password, type: 'sign-in' }),
+      })
+        .then((resp) => {
+          if (!resp.ok) reject('Ivalid email or password');
+
+          resolve('success');
+        })
+        .catch(() => reject('Ivalid email or password'));
+    }
+  );
+};
