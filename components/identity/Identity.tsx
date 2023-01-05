4ec1ccb9c8c3e5738bd4a4e7020a8decf61f685d
David <datogio@gmail.com>
2023-01-05 23:29:54 +0400

    feat: implement <Identity />

--- components/identity/Identity.tsx
+++ components/identity/Identity.tsx
@@ -1,5 +1,32 @@
-const Identity = () => {
-  return <div>Identity</div>;
+import { Avatar } from '../../stories/components/Avatar.stories';
+
+export interface IdentityProps {
+  firstName: string;
+  lastName: string;
+  details: string;
+  image: string;
+}
+
+const Identity = (props: IdentityProps) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <div>
+        <Avatar
+          firstName={props.firstName}
+          lastName={props.lastName}
+          image={props.image}
+          size="md"
+        />
+      </div>
+      <div>
+        <div className="flex space-x-1 font-bold text-sm">
+          <div>{props.firstName}</div>
+          <div>{props.lastName}</div>
+        </div>
+        <div className="text-xs opacity-60">{props.details}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Identity;
--- stories/components/Identity.stories.tsx
+++ stories/components/Identity.stories.tsx
@@ -1,8 +1,22 @@
 import { Identity as IdentityComponent } from '../../components';
+import fromImgToUrl from '../../__mocks__/fromImgToUrl';
+import type { IdentityProps } from '../../components/identity/Identity';
 
 export default {
   title: 'Components/Identity',
   component: IdentityComponent,
 };
 
-export const Identity = () => <IdentityComponent />;
+export const Identity = (args: IdentityProps) => (
+  <div className="w-[50%]">
+    <IdentityComponent {...args} />
+  </div>
+);
+
+const args: IdentityProps = {
+  firstName: 'David',
+  lastName: 'Giorgadze',
+  details: '2 hours ago',
+  image: fromImgToUrl(),
+};
+Identity.args = args;
