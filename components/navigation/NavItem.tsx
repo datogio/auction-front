f4f4a4b8acc5a6ea89ef56d9dd5dba867dc4c90e
David <datogio@gmail.com>
2023-01-06 14:51:40 +0400

    feat: implement <NavItem />

--- components/navigation/NavItem.tsx
+++ components/navigation/NavItem.tsx
@@ -1,5 +1,37 @@
-const NavItem = () => {
-  return <div>Nav Item</div>;
+import { ReactNode } from 'react';
+import { GoHome } from 'react-icons/go';
+import { FiSettings } from 'react-icons/fi';
+import { BsBookmarkDash } from 'react-icons/bs';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { FiHelpCircle } from 'react-icons/fi';
+
+export interface NavItemProps {
+  icon: 'settings' | 'home' | 'saved' | 'notifications' | 'help';
+  value: string;
+  active?: boolean;
+}
+
+const iconsMap: Record<NavItemProps['icon'], ReactNode> = {
+  home: <GoHome />,
+  settings: <FiSettings />,
+  saved: <BsBookmarkDash />,
+  notifications: <IoMdNotificationsOutline />,
+  help: <FiHelpCircle />,
+};
+
+const NavItem = (props: NavItemProps) => {
+  return (
+    <div
+      className={`${
+        props.active
+          ? 'bg-blue-600 text-white cursor-default shadow-lg'
+          : 'cursor-pointer'
+      } transition duration-200 py-2 rounded-md w-[100%] px-4 flex items-center space-x-2`}
+    >
+      {iconsMap[props.icon]}
+      <div>{props.value}</div>
+    </div>
+  );
 };
 
 export default NavItem;
--- stories/components/NavItem.stories.tsx
+++ stories/components/NavItem.stories.tsx
@@ -1,8 +1,17 @@
 import { NavItem as NavItemComponent } from '../../components';
+import type { NavItemProps } from '../../components/navigation/NavItem';
 
 export default {
   title: 'Components/NavItem',
   component: NavItemComponent,
 };
 
-export const NavItem = () => <NavItemComponent />;
+export const NavItem = (args: NavItemProps) => <NavItemComponent {...args} />;
+
+const args: NavItemProps = {
+  icon: 'home',
+  value: 'Home',
+  active: true,
+};
+
+NavItem.args = args;
