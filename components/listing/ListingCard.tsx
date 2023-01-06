e5e5be71d903429b55a5fef19f30f5026fb7415f
David <datogio@gmail.com>
2023-01-06 16:42:50 +0400

    feat: introduce <ListingCard />

--- components/listing/ListingCard.tsx
+++ components/listing/ListingCard.tsx
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { IdentityDetails } from '../../components';
 
 export interface ListingCardProps {
   href: string;
+  image: string;
+  title: string;
+  details: string;
 }
 
 const ListingCard = (props: ListingCardProps) => {
   return (
     <Link href={props.href}>
-      <a>Listing Card</a>
+      <a className="col-span-1 py-2 px-3 bg-white rounded-lg space-y-1 shadow-lg transition duration-200 hover:scale-105">
+        <div className="relative h-28 w-[100%] rounded-lg overflow-hidden">
+          <Image
+            src={props.image}
+            layout="fill"
+            objectFit="cover"
+            alt="Listing Image"
+          />
+        </div>
+        <IdentityDetails headline={props.title} details={props.details} />
+      </a>
     </Link>
   );
 };
--- stories/components/ListingCard.stories.tsx
+++ stories/components/ListingCard.stories.tsx
@@ -1,5 +1,6 @@
 import { ListinCard as ListingCardComponent } from '../../components';
 import type { ListingCardProps } from '../../components/listing/ListingCard';
+import fromImgToUrl from '../../__mocks__/fromImgToUrl';
 
 export default {
   title: 'Components/ListingCard',
@@ -7,11 +8,20 @@ export default {
 };
 
 export const ListingCard = (args: ListingCardProps) => (
-  <ListingCardComponent {...args} />
+  <div className="grid grid-cols-4 gap-5 bg-gray-100 p-2">
+    <ListingCardComponent {...args} />
+    <ListingCardComponent {...args} />
+    <ListingCardComponent {...args} />
+    <ListingCardComponent {...args} />
+    <ListingCardComponent {...args} />
+  </div>
 );
 
 const args: ListingCardProps = {
   href: '/',
+  image: fromImgToUrl(),
+  title: 'Listing Title',
+  details: '2 hours ago',
 };
 
 ListingCard.args = args;
