df3c736ecb0675982a240d945024b81b673132c1
David <datogio@gmail.com>
2023-01-07 14:38:33 +0400

    style: update <Overlay /> z index

--- components/overlay/Overlay.tsx
+++ components/overlay/Overlay.tsx
@@ -10,7 +10,7 @@ const Overlay = (props: OverlayProps) => {
   return (
     <motion.div
       {...overlayAnimation}
-      className="fixed z-50 top-0 bottom-0 left-0 right-0 blue-glassmorphism"
+      className="fixed z-40 top-0 bottom-0 left-0 right-0 blue-glassmorphism"
     >
       {props.children}
     </motion.div>
