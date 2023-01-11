c2e7bf1b4500cfb9640497263db0e763f202422f
David <datogio@gmail.com>
2023-01-11 17:35:45 +0400

    feat: clearing prompts on navigation to other page

--- components/navigation/NavItem.tsx
+++ components/navigation/NavItem.tsx
@@ -1,10 +1,12 @@
-import { ReactNode } from 'react';
-import Link from 'next/link';
+import { Dispatch, MouseEventHandler, ReactNode } from 'react';
+import { useRouter } from 'next/router';
 import { GoHome } from 'react-icons/go';
 import { FiSettings } from 'react-icons/fi';
 import { BsBookmarkDash, BsCart2 } from 'react-icons/bs';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { FiHelpCircle } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
+import * as promptActions from '../../store/prompt';
 
 export interface NavItemProps {
   href: string;
@@ -23,19 +25,26 @@ const iconsMap: Record<NavItemProps['icon'], ReactNode> = {
 };
 
 const NavItem = (props: NavItemProps) => {
+  const router = useRouter();
+  const dispatch: Dispatch<any> = useDispatch();
+
+  const onItemClick: MouseEventHandler<HTMLDivElement> = () => {
+    dispatch(promptActions.set([]));
+    router.push(props.href);
+  };
+
   return (
-    <Link href={props.href}>
-      <a
-        className={`${
-          props.active
-            ? 'bg-blue-600 text-white cursor-default shadow-lg'
-            : 'cursor-pointer text-gray-600 hover:bg-gray-100 hover:shadow-lg active:bg-transparent active:shadow-none'
-        } transition duration-200 py-2 rounded-md w-[100%] px-4 flex items-center space-x-2`}
-      >
-        {iconsMap[props.icon]}
-        <div className="text-sm">{props.value}</div>
-      </a>
-    </Link>
+    <div
+      onClick={onItemClick}
+      className={`${
+        props.active
+          ? 'bg-blue-600 text-white cursor-default shadow-lg'
+          : 'cursor-pointer text-gray-600 hover:bg-gray-100 hover:shadow-lg active:bg-transparent active:shadow-none'
+      } transition duration-200 py-2 rounded-md w-[100%] px-4 flex items-center space-x-2`}
+    >
+      {iconsMap[props.icon]}
+      <div className="text-sm">{props.value}</div>
+    </div>
   );
 };
 
