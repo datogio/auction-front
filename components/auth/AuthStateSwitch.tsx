a6853accf715450bddf20950ce38e00da95430c2
David <datogio@gmail.com>
2023-01-13 15:56:28 +0400

    fix: <AuthStateSwitch /> animation

--- components/auth/AuthStateSwitch.tsx
+++ components/auth/AuthStateSwitch.tsx
@@ -7,8 +7,11 @@ interface AuthStateSwitchProps {
 
 const AuthStateSwitch = (props: AuthStateSwitchProps) => {
   return (
-    <div className="flex justify-center text-white transition duration-200 hover:text-gray-200 hover:scale-x-110">
-      <div onClick={props.onCLick} className="cursor-pointer">
+    <div className="flex justify-center text-lg">
+      <div
+        onClick={props.onCLick}
+        className="cursor-pointer ml-2 text-white transition duration-200 hover:text-gray-200 hover:scale-125 active:scale-90"
+      >
         {props.value}
       </div>
     </div>
