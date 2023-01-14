4ad0b83fdd7e067dd5f9567fd9c246445df8f0ed
David <datogio@gmail.com>
2023-01-14 12:52:35 +0400

    refactor: time calculating logic

--- components/timeLeft/TimeLeft.tsx
+++ components/timeLeft/TimeLeft.tsx
@@ -6,24 +6,37 @@ export interface TimeLeftProps {
 
 const TimeLeft = (props: TimeLeftProps) => {
   const now = moment();
-  const end = moment(props.endDate);
-  const hours = end.diff(now, 'hours');
-  const milliSeconds = end.diff(now, 'millisecond');
-  const miliSecondsPerDay = parseInt((86400000 / 1000000).toString());
-  const miliSecondsLeft = parseInt((milliSeconds / 1000000).toString());
-  const miliSecondsPassed = miliSecondsPerDay - miliSecondsLeft;
+
+  const millisecondsLeft = () => {
+    const end = moment(props.endDate);
+    const milliSeconds = end.diff(now, 'millisecond');
+    const millisecondsLeft = parseInt((milliSeconds / 1000000).toString());
+    return millisecondsLeft;
+  };
+
+  const millisecondsPassed = () => {
+    const millisecondsPerDay = parseInt((86400000 / 1000000).toString());
+    const millisecondsPassed = millisecondsPerDay - millisecondsLeft();
+    return millisecondsPassed;
+  };
+
+  const hoursLeft = () => {
+    const end = moment(props.endDate);
+    const hours = end.diff(now, 'hours');
+    return hours;
+  };
 
   return (
     <div className="space-y-1 pt-1">
       <div className="flex bg-black w-[86px]">
-        {[...Array(miliSecondsLeft)].map((_, index) => (
+        {[...Array(millisecondsLeft())].map((_, index) => (
           <div key={index} className="bg-blue-600 h-1 w-[1px]"></div>
         ))}
-        {[...Array(miliSecondsPassed)].map((_, index) => (
+        {[...Array(millisecondsPassed())].map((_, index) => (
           <div key={index} className="bg-gray-400 h-1 w-[1px]"></div>
         ))}
       </div>
-      <div className="text-xs">{hours}hr left</div>
+      <div className="text-xs">{hoursLeft()}hr left</div>
     </div>
   );
 };
